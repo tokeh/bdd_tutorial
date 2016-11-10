9a7577360d6265697d5e62196d00ef1252dc438a
Tobias Keh <tobias.keh@googlemail.com>
2016-11-10 12:41:40 +0100

    Scalatest feature skeleton

--- src/test/scala/feature/BookSearch.scala
+++ src/test/scala/feature/BookSearch.scala
@@ -2,9 +2,24 @@ package feature
 
 import java.util
 
+import org.scalatest.{FeatureSpec, GivenWhenThen}
 import org.tokeh.bdd_tutorial.{Book, Library}
 
-class BookSearch {
+class BookSearch extends FeatureSpec with GivenWhenThen {
   private val library: Library = new Library
-  private var result = null
+  private var result: util.ArrayList[Book] = null
+
+  feature("Book search") {
+    info("To allow a customer to find his favourite books quickly, the library must offer multiple ways to search for a book")
+
+    scenario("Search books by publication year") {
+      Given("a book with the title 'One good book', written by 'Anonymous', published in 14 March 2013")
+      And("another book with the title 'Some other book', written by 'Tim Tomson', published in 23 August 2014")
+      And("another book with the title 'How to cook a dino', written by 'Fred Flintstone', published in 01 January")
+      When("the customer searches for books published between 2013 and 2014")
+      Then("2 books should have been found")
+      And("Book 1 should have the title 'Some other book'")
+      And("Book 2 should have the title 'One good book'")
+    }
+  }
 }
