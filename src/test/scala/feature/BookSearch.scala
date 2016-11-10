29f473a8a0a2189244ebbb4311d6983490ee0f5f
Tobias Keh <tobias.keh@googlemail.com>
2016-11-10 12:33:57 +0100

    Refactoring for plain scala testing

---
+++ src/test/scala/feature/BookSearch.scala
@@ -0,0 +1,10 @@
+package feature
+
+import java.util
+
+import org.tokeh.bdd_tutorial.{Book, Library}
+
+class BookSearch {
+  private val library: Library = new Library
+  private var result = null
+}
