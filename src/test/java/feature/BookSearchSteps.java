b805b79cee4d1cbcaed6d988ca0e5d37d05eac7d
Tobias Keh <tobias.keh@googlemail.com>
2016-11-10 09:08:56 +0100

    reduced to cucumber and java tests

--- src/test/java/feature/BookSearchSteps.java
+++ src/test/java/feature/BookSearchSteps.java
@@ -19,14 +19,14 @@ public class BookSearchSteps {
   private final Library library = new org.tokeh.bdd_tutorial.Library();
   private List<Book> result;
 
-  //@Given(".+book with the title '(.+)', written by '(.+)', published on (.+)")
+  @Given(".+book with the title '(.+)', written by '(.+)', published on (.+)")
   public void addNewBook(final String title, final String author, @Format("dd.MM.yyyy") final Date published) {
     LocalDate localDatePublished = published.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     org.tokeh.bdd_tutorial.Book book = new org.tokeh.bdd_tutorial.Book(title, author, localDatePublished);
     this.library.addBook(book);
   }
 
-  //@When("^the customer searches for books published between (\\d+) and (\\d+)$")
+  @When("^the customer searches for books published between (\\d+) and (\\d+)$")
   public void setSearchParameters(@Format("yyyy") final Date from, @Format("yyyy") final Date to) {
     LocalDate localDateFrom = from.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     LocalDate localDateTo = to.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
@@ -34,12 +34,12 @@ public class BookSearchSteps {
     result = library.findBooks(localDateFrom, localDateTo);
   }
 
-  //@Then("(\\d+) books should have been found$")
+  @Then("(\\d+) books should have been found$")
   public void verifyAmountOfBooksFound(final int booksFound) {
     assertEquals(result.size(), booksFound);
   }
 
-  //@And("Book (\\d+) should have the title '(.+)'$")
+  @And("Book (\\d+) should have the title '(.+)'$")
   public void verifyBookAtPosition(final int position, final String title) {
     assertEquals(result.get(position - 1).getTitle(), title);
   }
