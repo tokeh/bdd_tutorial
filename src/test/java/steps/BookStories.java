92c59cbc62a3e4f4c5ade5ac6e13d7b2af2607b0
Tobias Keh <tobias.keh@googlemail.com>
2016-11-10 15:24:38 +0100

    refactoring

--- src/test/java/steps/BookStories.java
+++ src/test/java/steps/BookStories.java
@@ -6,16 +6,12 @@ import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.JUnitStories;
-import org.jbehave.core.junit.JUnitStory;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
-import steps.BookSearchSteps;
 
 import java.util.List;
-
-import static org.jbehave.core.io.CodeLocations.*;
 
 public class BookStories extends JUnitStories {
   @Override
@@ -24,7 +20,6 @@ public class BookStories extends JUnitStories {
         "**/*.story", "**/exclude_*.story");
   }
 
-  // Here we specify the configuration, starting from default MostUsefulConfiguration, and changing only what is needed
   @Override
   public Configuration configuration() {
     return new MostUsefulConfiguration()
@@ -33,10 +28,8 @@ public class BookStories extends JUnitStories {
             .withFormats(Format.XML, Format.IDE_CONSOLE, Format.CONSOLE, Format.HTML, Format.TXT));
   }
 
-  // Here we specify the steps classes
   @Override
   public InjectableStepsFactory stepsFactory() {
-    // varargs, can have more that one steps classes
     return new InstanceStepsFactory(configuration(), new BookSearchSteps());
   }
 }
