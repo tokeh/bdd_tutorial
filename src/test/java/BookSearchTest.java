451fd8d171a465c414746ab28df466f63d511991
tokeh <tobias.keh@googlemail.com>
2016-11-14 15:48:10 +0100

    Cucumber reporting added

--- src/test/java/BookSearchTest.java
+++ src/test/java/BookSearchTest.java
@@ -1,8 +1,29 @@
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty")
-public class BookSearchTest {
-}
+@RunWith(ExtendedCucumber.class)
+
+@ExtendedCucumberOptions(
+        jsonReport = "target/cucumber/cucumber.json",
+        overviewReport = true,
+        usageReport = true,
+        detailedReport = true,
+        detailedAggregatedReport = true,
+        featureOverviewChart = true,
+        retryCount = 0,
+        coverageReport = true,
+        excludeCoverageTags = {"@flaky" },
+        includeCoverageTags = {"@passed" },
+        overviewChartsReport = true,
+        outputFolder = "target/cucumber")
+
+@CucumberOptions(plugin = {
+        "html:target/cucumber/cucumber-html-report",
+        "json:target/cucumber/cucumber.json",
+        "pretty:target/cucumber/cucumber-pretty.txt",
+        "usage:target/cucumber/cucumber-usage.json",
+        "junit:target/cucumber/cucumber-results.xml"})
+
+public class BookSearchTest { }
