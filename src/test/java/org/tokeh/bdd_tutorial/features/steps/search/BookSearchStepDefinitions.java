82102ad38fa008371c1be6244d58bac2da2875e6
Tobias Keh <tobias.keh@googlemail.com>
2016-11-17 16:36:59 +0100

    Step definitions for second scenario added to show tag functionality

--- src/test/java/org/tokeh/bdd_tutorial/features/steps/search/BookSearchStepDefinitions.java
+++ src/test/java/org/tokeh/bdd_tutorial/features/steps/search/BookSearchStepDefinitions.java
@@ -1,5 +1,7 @@
 package org.tokeh.bdd_tutorial.features.steps.search;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -11,7 +13,7 @@ public class BookSearchStepDefinitions {
   @Steps
   BookSearchSteps bookSearch;
 
-  @Given(".+book with the title '(.+)', written by '(.+)', published on (.+)")
+  @Given(".+book with the title '(.+)', written by '(.+)', published on (.+)$")
   public void addNewBook(final String title, final String author, final String published) {
     this.bookSearch.addNewBook(title, author, published);
   }
@@ -29,5 +31,25 @@ public class BookSearchStepDefinitions {
   @And("Book (\\d+) should have the title '(.+)'$")
   public void verifyBookAtPosition(final int position, final String title) {
     this.bookSearch.verifyBookAtPosition(position, title);
+  }
+
+  @When("^the customer searches books that have been written by '(.+)'$")
+  public void searchByAuthor(final String author) {
+    this.bookSearch.searchByAuthor(author);
+  }
+
+  @Then("^Book (\\d+) should be written by '(.+)'$")
+  public void verifyAuthorAtPosition(final int position, final String author) {
+    this.bookSearch.verifyAuthorAtPosition(position, author);
+  }
+
+  @Before("@test#1")
+  public void beforeScenario() {
+    System.out.println("This happened before scenario 'Search books by publication year'");
+  }
+
+  @After("@test#2")
+  public void afterScenario() {
+    System.out.println("This happened after scenario 'Search books by author'");
   }
 }
