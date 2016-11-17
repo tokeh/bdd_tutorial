9f1cf68cc9bf5a4b094acb113ec463c0d2fa9636
Tobias Keh <tobias.keh@googlemail.com>
2016-11-17 14:27:54 +0100

    formatting

--- src/test/java/BookSearchTest.java
+++ src/test/java/BookSearchTest.java
@@ -6,24 +6,24 @@ import org.junit.runner.RunWith;
 @RunWith(ExtendedCucumber.class)
 
 @ExtendedCucumberOptions(
-        jsonReport = "target/cucumber/cucumber.json",
-        overviewReport = true,
-        usageReport = true,
-        detailedReport = true,
-        detailedAggregatedReport = true,
-        featureOverviewChart = true,
-        retryCount = 0,
-        coverageReport = true,
-        excludeCoverageTags = {"@flaky" },
-        includeCoverageTags = {"@passed" },
-        overviewChartsReport = true,
-        outputFolder = "target/cucumber")
+    jsonReport = "target/cucumber/cucumber.json",
+    overviewReport = true,
+    usageReport = true,
+    detailedReport = true,
+    detailedAggregatedReport = true,
+    featureOverviewChart = true,
+    retryCount = 0,
+    coverageReport = true,
+    excludeCoverageTags = {"@flaky" },
+    includeCoverageTags = {"@passed" },
+    overviewChartsReport = true,
+    outputFolder = "target/cucumber")
 
 @CucumberOptions(plugin = {
-        "html:target/cucumber/cucumber-html-report",
-        "json:target/cucumber/cucumber.json",
-        "pretty:target/cucumber/cucumber-pretty.txt",
-        "usage:target/cucumber/cucumber-usage.json",
-        "junit:target/cucumber/cucumber-results.xml"})
+    "pretty",
+    "html:target/cucumber/cucumber-html-report",
+    "json:target/cucumber/cucumber.json",
+    "usage:target/cucumber/cucumber-usage.json",
+    "junit:target/cucumber/cucumber-results.xml"})
 
 public class BookSearchTest { }
