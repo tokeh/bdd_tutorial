d1ef8f0add42783277aa2cd6ba54e7492b1baa45
Tobias Keh <tobias.keh@googlemail.com>
2016-11-18 14:36:23 +0100

    Cucumber tags added

--- src/test/java/org/tokeh/bdd_tutorial/features/steps/manage/ManageTests.java
+++ src/test/java/org/tokeh/bdd_tutorial/features/steps/manage/ManageTests.java
@@ -5,5 +5,9 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/manage/manage_books.feature")
+@CucumberOptions(
+    features = "src/test/resources/features/manage/manage_books.feature",
+    plugin = "pretty",
+    tags = {}
+)
 public class ManageTests { }
--- src/test/java/org/tokeh/bdd_tutorial/features/steps/search/SearchTests.java
+++ src/test/java/org/tokeh/bdd_tutorial/features/steps/search/SearchTests.java
@@ -5,5 +5,9 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/search/book_search.feature")
+@CucumberOptions(
+    features = "src/test/resources/features/search/book_search.feature",
+    plugin = "pretty",
+    tags = {"@test#1"}
+)
 public class SearchTests { }
