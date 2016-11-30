296b0a4dfea7525a34705bfbc0d4be856a6dd3ab
Tobias Keh <tobias.keh@googlemail.com>
2016-11-30 14:23:21 +0100

    versions of Cucumber and JBehave aligned

--- src/main/java/org/tokeh/bdd_tutorial/Library.java
+++ src/main/java/org/tokeh/bdd_tutorial/Library.java
@@ -1,16 +1,13 @@
 package org.tokeh.bdd_tutorial;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Library {
   private final HashMap<String, Book> books;
 
   public Library() {
-    this.books = new HashMap<String, Book>();
+    this.books = new HashMap<>();
   }
 
   public void addBook(final Book book) {
@@ -18,23 +15,23 @@ public class Library {
   }
 
   public ArrayList<Book> findBooks(final LocalDate from, final LocalDate to) {
-    ArrayList<Book> list = new ArrayList<Book>();
+    ArrayList<Book> list = new ArrayList<>();
     for (Map.Entry entry : this.books.entrySet()) {
       Book book = (Book) entry.getValue();
 
       if (book.getPublished().getYear() >= from.getYear()
-        && book.getPublished().getYear() <= to.getYear()) {
+          && book.getPublished().getYear() <= to.getYear()) {
         list.add(book);
       }
     }
 
-    Collections.sort(list, (Book book1, Book book2) ->
+    list.sort((Book book1, Book book2) ->
         Integer.compare(book2.getPublished().getYear(), book1.getPublished().getYear()));
     return list;
   }
 
   public ArrayList<Book> findBooksByAuthor(final String author) {
-    ArrayList<Book> list = new ArrayList<Book>();
+    ArrayList<Book> list = new ArrayList<>();
     for (Map.Entry entry : this.books.entrySet()) {
       Book book = (Book) entry.getValue();
 
@@ -43,8 +40,34 @@ public class Library {
       }
     }
 
-    Collections.sort(list, (Book book1, Book book2) ->
+    list.sort((Book book1, Book book2) ->
         Integer.compare(book2.getPublished().getYear(), book1.getPublished().getYear()));
+    return list;
+  }
+
+  public List<String> searchAuthorByName(final String author) {
+    ArrayList<String> list = new ArrayList<>();
+    for (Map.Entry entry : this.books.entrySet()) {
+      Book book = (Book) entry.getValue();
+
+      if (book.getAuthor().equals(author)) {
+        list.add(book.getAuthor());
+      }
+    }
+
+    return list;
+  }
+
+  public ArrayList<String> searchForAllAuthors() {
+    Set<String> set = new HashSet<>();
+    for (Map.Entry entry : this.books.entrySet()) {
+      Book book = (Book) entry.getValue();
+
+      set.add(book.getAuthor());
+    }
+
+    ArrayList<String> list = new ArrayList<>(set);
+    Collections.sort(list);
     return list;
   }
 }
